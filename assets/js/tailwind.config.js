f1368cfa217b28f15699aa96abcdcf0d35c2a139
Michael Amiro <odindo.michael@gmail.com>
2022-10-27 20:32:21 +0300

    Added landing homepage

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+	theme: {
+		extend: {
+			colors: {
+				primary: "#da373d",
+				charcoal: {
+					500: "#4B6874",
+					800: "#101E23",
+					900: "#264653",
+				},
+				black: "#161616",
+				seashell: "#F3EAE5",
+				"persian-green": "#2A9D8F",
+			},
+			fontFamily: {
+				sans: ["Inder", "sans-serif"],
+				serif: ["Judson", "serif"],
+				alternate: ["Work Sans", "sans-serif"],
+			},
+		},
+	},
+}
